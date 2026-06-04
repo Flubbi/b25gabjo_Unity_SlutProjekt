67107fef82e246771036b0dcd10c46490bd8e861
Flubbi <b25gabjo@student.his.se>
2026-06-04 04:27:16 +0200

    Spitter attack script change

--- Assets/2DGamekit/Scripts/Character/MonoBehaviours/EnemyBehaviour.cs
+++ Assets/2DGamekit/Scripts/Character/MonoBehaviours/EnemyBehaviour.cs
@@ -399,6 +399,10 @@ namespace Gamekit2D
             m_FireTimer = 1.0f;
         }
 
+        public void ReadyShoot()
+        { 
+            audioManager.PlayEnemyRanged(gameObject);
+        }
         public void Shooting()
         {
             Vector2 shootPosition = shootingOrigin.transform.localPosition;
@@ -409,8 +413,6 @@ namespace Gamekit2D
 
             BulletObject obj = m_BulletPool.Pop(shootingOrigin.TransformPoint(shootPosition));
             
-            audioManager.PlayEnemyRanged(gameObject);
-
             obj.rigidbody2D.linearVelocity = (GetProjectilVelocity(m_TargetShootPosition, shootingOrigin.transform.position));
         }
 
